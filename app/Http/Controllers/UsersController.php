ee694f5125240beaf7319d85c50cbd3614b9c77e
aoding9 <xy5217733@163.com>
2022-02-26 00:39:01 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,19 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        // 除了show,create,store 其他请求必须要登录才可访问
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+//只让未登录用户访问注册页面：
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -39,25 +52,27 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
-        $this->validate($request,[
-            'name'=>'required|max:50',
-            'password'=>'nullable|confirmed|min:6'
+        $this->authorize('update', $user);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
         ]);
 
-        $data=[];
-        $data['name']=$request->name;
-        if($request->password){
-            $data['password']=$request->password;
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = $request->password;
         }
 
         $user->update($data);
 
-        session()->flash('success','个人资料更新成功！');
-        return redirect()->route('users.show',$user->id);
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user->id);
     }
 }

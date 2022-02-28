f5e258cf8df8b2b344f4bf5b2ce1d59ccbec82d0
aoding9 <xy5217733@163.com>
2022-02-28 23:46:07 +0800

    用户微博列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        // unguard方法可以临时关闭模型的fillable批量复制保护，reguard方法再次开启批量赋值保护
+// https://blog.csdn.net/jsntdhf/article/details/90956167
         Model::unguard();
 
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+
 
         Model::reguard();
     }

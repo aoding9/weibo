3b92fefeb21271a615263bc1fae02a9957824aee
aoding9 <xy5217733@163.com>
2022-04-10 09:16:03 +0800

    删除微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,23 +7,23 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    public function home()
-    {
-        $feed_items = [];
-        if (Auth::check()) {
-            $feed_items = Auth::user()->feed()->paginate(30);
-        }
-
-        return view('static_pages/home', compact('feed_items'));
+  public function home()
+  {
+    $feed_items = [];
+    if (Auth::check()) {
+      $feed_items = Auth::user()->feed()->paginate(30);
     }
 
-    public function help()
-    {
-        return view('static_pages/help');
-    }
+    return view('static_pages/home', compact('feed_items'));
+  }
 
-    public function about()
-    {
-        return view('static_pages/about');
-    }
+  public function help()
+  {
+    return view('static_pages/help');
+  }
+
+  public function about()
+  {
+    return view('static_pages/about');
+  }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,65 +11,65 @@ use Str;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-    protected $table = 'users';
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	use HasApiTokens, HasFactory, Notifiable;
+	protected $table = 'users';
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    public function gravatar($size = '100')
-    {
-        $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
-    }
-
-
-    public static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($user) {
-            $user->activation_token = Str::random(10);
-        });
-    }
-    public function statuses()
-    {
-        return $this->hasMany(Status::class);
-    }
+	public function gravatar($size = '100')
+	{
+		$hash = md5(strtolower(trim($this->attributes['email'])));
+		return "http://www.gravatar.com/avatar/$hash?s=$size";
+	}
 
 
-    /**
-     * 获取用户最新发布的微博动态
-     */
-    public function feed()
-    {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
-    }
+	public static function boot()
+	{
+		parent::boot();
+
+		static::creating(function ($user) {
+			$user->activation_token = Str::random(10);
+		});
+	}
+	public function statuses()
+	{
+		return $this->hasMany(Status::class);
+	}
+
+
+	/**
+	 * 获取用户最新发布的微博动态
+	 */
+	public function feed()
+	{
+		return $this->statuses()
+			->orderBy('created_at', 'desc');
+	}
 }
